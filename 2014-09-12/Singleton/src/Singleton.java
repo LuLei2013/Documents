4f9b0fd08c8a077c39900f41a37d51256d91dc06
ELELULE <lei.a.lu@ericsson.com>
2014-09-12 19:12:44 +0800

    [2014-09-12 Singleton Enum]

---
+++ 2014-09-12/Singleton/src/Singleton.java
@@ -0,0 +1,11 @@
+/**
+ * @author lulei lei.a.lu@ericsson.com
+ * @version Sep 12, 2014 6:50:08 PM
+ */
+public enum Singleton {
+	INSTANCE;
+	public void test() {
+		System.out.println("test method");
+	}
+
+}
---
+++ 2014-09-12/Singleton/src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * @author lulei lei.a.lu@ericsson.com
+ * @version Sep 12, 2014 6:51:14 PM
+ */
+public class Test {
+	public static void main(String[] args) {
+		Singleton.INSTANCE.test();
+	}
+}
